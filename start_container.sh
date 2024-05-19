93fcc44796163c02ea8c7fc5f52613407e037938
harish667-hash <harishharish71902@gmail.com>
2024-05-19 18:11:50 +0530

    changes

--- start_container.sh
+++ start_container.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 # Pull the Docker image from Docker Hub
-docker pull harish552/sample:latest
+docker pull harish552/sample
 
 
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 harish552/sample:latest
+docker run -d -p 5000:5000 harish552/sample
